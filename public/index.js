f0844373854b7f6445ddacbc02e7823a250d1dba
Habeeb-bee <habeebirving11@gmail.com>
2024-02-13 01:49:02 +0100

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,16 @@
+// const meanu = document.getElementsByTagName('menu');
+
+
+
+
+
+// const onToggleMenu = function(e) {
+    
+// }
+
+// onToggleMenu(console.log(meanu.class)) 
+
+// // menu.addEventListener('click',(e) =>{
+// //     console.log(e);
+// //     onToggleMenu() 
+// // } )
