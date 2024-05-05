c7e70760aff6eab2e39d49049f48b5cb481cd090
Habeeb-bee <138727069+Habeeb-bee@users.noreply.github.com>
2024-05-05 01:02:16 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { 
+      animation:{
+      "loop-scroll": "loop-scroll 8s linear infinite",
+    },
+      keyframes: {
+      "loop-scroll": {
+        from: { transform: "translateX(0)" },
+        to: { transform: "translateX(-100%)" },
+      },
+    },
+
+    fontFamily: {
+      "head": ["dancing", "sans-serif"],
+      "body": ["josefinsans"]
+    }
+
+  },
   },
   plugins: [],
 }
